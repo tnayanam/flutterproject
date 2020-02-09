93e0ac67043ec8a04fed4331c4e5761e404f7464
Tanuj Nayanam <tnayanam@sessionm.com>
2020-02-09 13:13:53 -0500

    Add some separation between menu items and separationfrom all edges of websites

--- lib/navbar/navbar.dart
+++ lib/navbar/navbar.dart
@@ -19,22 +19,38 @@ class Navbar extends StatelessWidget {
 class DesktopNavbar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Row(
-        children: <Widget>[
-          Text(
-            'Retro Protal Studio',
-            style: TextStyle(
-                fontWeight: FontWeight.bold, color: Colors.white, fontSize: 30),
-          ),
-          Row(
-            children: <Widget>[
-              Text("Home", style: TextStyle(color: Colors.white)),
-              Text("AboutUs", style: TextStyle(color: Colors.white)),
-              Text("Portfolio", style: TextStyle(color: Colors.white))
-            ],
-          )
-        ],
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 40),
+      child: Container(
+        //constraints: BoxConstraints(maxWidth: 1200),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Text(
+              'Retro Protal Studio',
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                  fontSize: 30),
+            ),
+            Row(
+              children: <Widget>[
+                Text("Home", style: TextStyle(color: Colors.white)),
+                SizedBox(
+                  width: 30,
+                ),
+                Text("AboutUs", style: TextStyle(color: Colors.white)),
+                SizedBox(
+                  width: 30,
+                ),
+                Text("Portfolio", style: TextStyle(color: Colors.white)),
+                SizedBox(
+                  width: 30,
+                ),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
