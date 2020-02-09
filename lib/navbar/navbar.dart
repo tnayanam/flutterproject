67eea421b0f29613fb762ab759f8ddd365a3c14d
Tanuj Nayanam <tnayanam@sessionm.com>
2020-02-09 14:04:26 -0500

    padding in navbar

--- lib/navbar/navbar.dart
+++ lib/navbar/navbar.dart
@@ -27,7 +27,7 @@ class DesktopNavbar extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: <Widget>[
             Text(
-              'Retro Protal Studio',
+              'Kinsvilla Retro Protal Studio',
               style: TextStyle(
                   fontWeight: FontWeight.bold,
                   color: Colors.white,
@@ -72,6 +72,33 @@ class MobileNavbar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+        child: Padding(
+      padding: const EdgeInsets.symmetric(vertical: 20, horizontal: 40),
+      child: Column(children: <Widget>[
+        Text(
+          'Kinsvilla Retro Protal Studio',
+          style: TextStyle(
+              fontWeight: FontWeight.bold, color: Colors.white, fontSize: 30),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text("Home", style: TextStyle(color: Colors.white)),
+              SizedBox(
+                width: 30,
+              ),
+              Text("AboutUs", style: TextStyle(color: Colors.white)),
+              SizedBox(
+                width: 30,
+              ),
+              Text("Portfolio", style: TextStyle(color: Colors.white)),
+            ],
+          ),
+        ),
+      ]),
+    ));
   }
 }
